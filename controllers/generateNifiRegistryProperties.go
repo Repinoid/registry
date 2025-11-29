b2200b9bab6d6a10a58e0fc3048cb96e095d8d8a
“Naeel” <tazet@narod.ru>
2025-11-29 08:14:15 +0400

    // (Версия 13.0: Чистый KeyValue Provider + Удаление всех DB настроек H2 и identity-providers.xml)

--- controllers/generateNifiRegistryProperties.go
+++ controllers/generateNifiRegistryProperties.go
@@ -6,7 +6,7 @@ import (
 	registryv1 "github.com/repinoid/nreg-oper/api/v1"
 )
 
-/// (Версия 12.0: Финальная фиксация H2 - Возврат всех 4 полей H2 с фиктивным паролем)
+// (Версия 13.0: Чистый KeyValue Provider + Удаление всех DB настроек H2 и identity-providers.xml)
 // generateNifiRegistryProperties генерирует содержимое nifi-registry.properties
 func generateNifiRegistryProperties(nifiRegistry *registryv1.NifiRegistry, dbPassword string) string {
 	var effectiveFlowProvider string
@@ -26,15 +26,9 @@ nifi.registry.db.password=%s`,
 			nifiRegistry.Spec.Database.Username, dbPassword)
 	} else {
 		// КОНФИГУРАЦИЯ ДЛЯ H2 (Встроенная БД)
-		// Устанавливаем ВСЕ 4 поля с фиктивными, но необходимыми для синтаксиса H2 значениями.
+		// Устанавливаем ТОЛЬКО KeyValue Provider. Полностью исключаем nifi.registry.db.*.
 		effectiveFlowProvider = "org.apache.nifi.registry.flow.keyvalue.KeyValueFlowProvider"
-		dbConfigSection = `
-# Database Configuration (H2 - Embedded)
-nifi.registry.db.implementation=org.apache.nifi.registry.db.sql.SqlFlowPersistenceProvider
-nifi.registry.db.url=jdbc:h2:./database/nifi-registry-db
-nifi.registry.db.driver.class=org.h2.Driver
-nifi.registry.db.username=sa
-nifi.registry.db.password=h2password` 
+		dbConfigSection = "" 
 	}
 
 	return fmt.Sprintf(`
@@ -57,7 +51,6 @@ nifi.registry.web.api.context.path=/nifi-registry-api
 nifi.registry.web.jetty.threads=200
 
 # Security Settings
-# ВРЕМЕННО ОТКЛЮЧЕНО
 # nifi.registry.security.user.login.identity.provider=keycloak 
 nifi.registry.security.user.login.identity.provider=
 
@@ -72,7 +65,8 @@ nifi.registry.security.user.group.provider.implementation.org.apache.nifi.regist
 nifi.registry.security.user.group.provider.implementation.org.apache.nifi.registry.security.authorization.file.FileUserGroupProvider.initial.admin.identity=
 
 # Identity Providers Settings
-nifi.registry.security.identity.providers.file=./conf/identity-providers.xml
+# nifi.registry.security.identity.providers.file=./conf/identity-providers.xml <--- КОММЕНТИРУЕМ ЭТУ СТРОКУ
+# nifi.registry.security.identity.providers.file=./conf/identity-providers.xml
 
 # Notification Service Settings
 nifi.registry.notification.services.file=./conf/notification-services.xml
